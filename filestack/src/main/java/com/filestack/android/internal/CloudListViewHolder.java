b0220e031fe8e9e2936ea320e4dc9ed8d50f9ae0
Talha Javed <tjaved@fieldwire.com>
2021-04-15 11:32:01 -0500

    replacing picasso with Glide

--- filestack/src/main/java/com/filestack/android/internal/CloudListViewHolder.java
+++ filestack/src/main/java/com/filestack/android/internal/CloudListViewHolder.java
@@ -1,8 +1,6 @@
 package com.filestack.android.internal;
 
-import android.content.Context;
 import android.content.res.ColorStateList;
-import androidx.annotation.ColorInt;
 import androidx.core.graphics.ColorUtils;
 import androidx.core.widget.ImageViewCompat;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,9 +8,9 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.filestack.android.R;
 import com.filestack.android.Theme;
-import com.squareup.picasso.Picasso;
 
 /**
  * Holds references to view elements inside {{@link RecyclerView}} list items.
@@ -60,7 +58,7 @@ class CloudListViewHolder extends RecyclerView.ViewHolder {
 
     public void setIcon(String url) {
         if (iconView != null) {
-            Picasso.get().load(url).into(iconView);
+            Glide.with(iconView.getContext()).load(url).into(iconView);
         }
     }
 
